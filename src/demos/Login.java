584ba61547d937e207edcf15787ad9a83810b540
Yuriy <Seaman_on_watch@mail.ru>
2019-06-30 22:01:23 +0300

    Simulating User Actions on a Web Page

--- src/demos/Login.java
+++ src/demos/Login.java
@@ -1,5 +1,6 @@
 package demos;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,7 +11,16 @@ public class Login {
         System.setProperty("webdriver.chrome.driver", "sf/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-//        2. Open web browser and navigate to page
+        //2. Open web browser and navigate to page
         driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
+
+        //3. Enter email address
+        driver.findElement(By.name("ctl00$MainContent$txtUserName")).sendKeys("tim@testemail.com");
+
+        //4. Enter password
+        driver.findElement(By.name("ctl00$MainContent$txtPassword")).sendKeys("trpass");
+
+        //5. Click login
+        driver.findElement(By.name("ctl00$MainContent$btnLogin")).click();
     }
 }
