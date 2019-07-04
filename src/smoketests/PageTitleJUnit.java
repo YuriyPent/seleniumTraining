27ef6f7b080393600cff6103d06a9c6bfcde2107
Yuriy <Seaman_on_watch@mail.ru>
2019-07-04 19:53:15 +0300

    Annotations: Guiding the Test Flow

--- src/smoketests/PageTitleJUnit.java
+++ src/smoketests/PageTitleJUnit.java
@@ -1,6 +1,8 @@
 package smoketests;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
@@ -9,6 +11,7 @@ public class PageTitleJUnit {
     @Test
     public void pageTitleTest(){
 
+        System.out.println("Running the test");
         String webURL = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
         WebDriver driver = utilities.DriverFactory.open("chrome");
         driver.get(webURL);
@@ -16,5 +19,15 @@ public class PageTitleJUnit {
         System.out.println("ACTUAL TITLE IS: " + actualTitle);
         String expectedTitle = "SDET Training | Account Management";
         Assert.assertEquals(expectedTitle, actualTitle);
+    }
+
+    @Before
+    public void setUp(){
+        System.out.println("Setting up the test");
+    }
+
+    @After
+    public void tearDown(){
+        System.out.println("Closing the test");
     }
 }
