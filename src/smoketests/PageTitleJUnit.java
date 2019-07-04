a7dd4465ecbfb22185acf2630d557bd885fcb3f4
Yuriy <Seaman_on_watch@mail.ru>
2019-07-04 20:11:45 +0300

    Class Variables within a Framework

--- src/smoketests/PageTitleJUnit.java
+++ src/smoketests/PageTitleJUnit.java
@@ -8,12 +8,14 @@ import org.openqa.selenium.WebDriver;
 
 public class PageTitleJUnit {
 
+//  Declare variables and objects at CLASS level in order to access in multiple methods through program
+    private WebDriver driver;
+    private String webURL = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
+
     @Test
     public void pageTitleTest(){
 
         System.out.println("Running the test");
-        String webURL = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
-        WebDriver driver = utilities.DriverFactory.open("chrome");
         driver.get(webURL);
         String actualTitle = driver.getTitle();
         System.out.println("ACTUAL TITLE IS: " + actualTitle);
@@ -23,11 +25,13 @@ public class PageTitleJUnit {
 
     @Before
     public void setUp(){
-        System.out.println("Setting up the test");
+        System.out.println("Initializing the driver");
+        driver = utilities.DriverFactory.open("chrome");
     }
 
     @After
     public void tearDown(){
-        System.out.println("Closing the test");
+        System.out.println("Closing the driver");
+        driver.close();
     }
 }
