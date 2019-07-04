b88b774e46adedcea3d9b39e6c133d4e2e58d4fa
Yuriy <Seaman_on_watch@mail.ru>
2019-07-04 16:17:27 +0300

    Cross-Browser Testing

--- src/demos/NewAccount.java
+++ src/demos/NewAccount.java
@@ -1,26 +1,43 @@
 package demos;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class NewAccount {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
         String name = "Mary Smith";
-        String email ="ms@testemail.com";
+        String email = "ms@testemail.com";
         String password = "mspass";
-        String gender;
         String country = "United States";
         String phoneNumber = "123456789";
+
+        String gender;
         String weeklyEmail;
         String monthlyEmail;
         String occassionalEmail;
 
+        String browserType = "chrome";
+        WebDriver driver;
+
 //        1. Create WebDriver
-        System.setProperty("webdriver.chrome.driver", "sf/chromedriver.exe");
-        org.openqa.selenium.WebDriver driver = new ChromeDriver();
+        if (browserType.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "sf/chromedriver.exe");
+            driver = new ChromeDriver();
+            System.out.println("Using Chrome");
+        } else if (browserType.equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "sf/geckodriver.exe");
+            driver = new FirefoxDriver();
+            System.out.println("Using FF");
+        } else {
+            System.setProperty("webdriver.chrome.driver", "sf/chromedriver.exe");
+            driver = new ChromeDriver();
+            System.out.println("Using Chrome");
+        }
 
 //        2. Open browser to account page --> click create account
         driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
