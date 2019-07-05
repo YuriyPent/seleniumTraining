3b9d4b561257f1a526bda956d949e9458b07698c
Yuriy <Seaman_on_watch@mail.ru>
2019-07-05 22:56:22 +0300

    Writing the Search Page Test Step

--- src/tests/Expedia.java
+++ src/tests/Expedia.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,12 +11,22 @@ public class Expedia {
 
     private WebDriver driver;
     private String browserType = "chrome";
+    private String city = "New York, New York";
+    private String checkIn = "07/07/2019";
+    private String checkOut = "07/14/2019";
+//    private String numOfGuest = "2";
 
     @Test
     public void hotelReservation() {
 
 //        1. Search
-
+            driver.findElement(By.id("tab-hotel-tab-hp")).click();
+            driver.findElement(By.id("hotel-destination-hp-hotel")).sendKeys(city);
+            driver.findElement(By.id("hotel-checkin-hp-hotel")).sendKeys(checkIn);
+            driver.findElement(By.id("hotel-checkout-hp-hotel")).sendKeys(checkOut);
+//            new Select(driver.findElement(By.xpath("//*[@id=\"gcw-hotel-form-hp-hotel\"]/div[4]/div[4]/label/select")))
+//                    .selectByValue(numOfGuest);
+            driver.findElement(By.xpath("//*[@id=\"gcw-hotel-form-hp-hotel\"]/div[10]/label/button")).click();
 //        2. Modify the search results page, give criteria
 
 //        3. Analyze the results and make our selection
