995282f914ae36529983b47f143ac813d388eb3a
Yuriy <Seaman_on_watch@mail.ru>
2019-07-12 17:04:18 +0300

    Implementing Step Definitions with Selenium

--- src/stepImplementations/BDDLoginTest.java
+++ src/stepImplementations/BDDLoginTest.java
@@ -3,23 +3,37 @@ package stepImplementations;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utilities.DriverFactory;
 
 public class BDDLoginTest {
+    private WebDriver driver;
 
-//    WebDriver driver;
+    private String username = "tim@testemail.com";
+    private String password = "trpass";
 
     @Given("^user is on the login page$")
     public void user_is_on_the_login_page() {
+
         System.out.println("User is on the login page");
+//        open driver
+        driver = DriverFactory.open("chrome");
+        driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
     }
 
     @When("^user enters correct username and correct password$")
     public void user_enters_correct_username_and_correct_password() {
         System.out.println("User enters correct username and correct password");
+        driver.findElement(By.id("MainContent_txtUserName")).sendKeys(username);
+        driver.findElement(By.id("MainContent_txtPassword")).sendKeys(password);
+        driver.findElement(By.id("MainContent_btnLogin")).click();
     }
 
     @Then("^user gets confirmation$")
     public void user_gets_confirmation() {
         System.out.println("User gets confirmation");
+        Assert.assertTrue(driver.findElement(By.id("conf_message")).getText().contains("success"));
     }
 }
